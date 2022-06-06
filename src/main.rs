0efbcb71e4870f7a19c925bec1494c0fea256d6b
Jorge luis Montoya <31372667+koke07@users.noreply.github.com>
2022-06-06 10:39:07 -0500

    mis cambios

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 fn main() {
-    let mut nombres: Vec<String> = Vec::new();
+
+    let mut nombres : Vec<String> = Vec::new();
+
     for i in 0..3 {
         println!("Por favor introduce un nombre: ");
         let mut nombre = String::new();
         std::io::stdin().read_line(&mut nombre).unwrap();
+    
         nombres.push(nombre);
+    } 
+
+    for nombre in nombres {
+        println!("El nombre es: {}", nombre);
     }
-    for nombre in nombres{
-        print!("El nombre es:{}",nombre);
-    }
-   // print!("{:?}",nombres)
+
+    let hola = ["H", "O", "L", "A"];
+
+    println!("{}", hola[0]);
+    println!("{}", hola[1]);
+    println!("{}", hola[2]);
+    println!("{}", hola[3]);
+
+    // println!("{}", hola[4]);
 }
